4a2cbd24e8cc064fb4a8a206c41ad786cc04ec54
Tanmay <tanmayshah96@gmail.com>
2017-07-07 15:11:46 +0530

    Editing in header of invoicetron webpage

--- invoicetron/settings.py
+++ invoicetron/settings.py
@@ -136,6 +136,8 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
+
+
 AWS_HEADERS = {
   'Cache-Control': 'max-age=86400',
 }
